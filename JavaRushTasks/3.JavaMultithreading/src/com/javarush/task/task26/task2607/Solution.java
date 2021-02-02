322001306d1cc88b8d66628104e3f4922cd6642b
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-02 21:37:32 +0500

    Solution of the Task2607

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task26/task2607/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task26/task2607/Solution.java
@@ -5,6 +5,19 @@ package com.javarush.task.task26.task2607;
 */
 
 public class Solution {
+    public static class IntegerHolder{
+        private int value;
+
+        public synchronized int get() {
+            return value;
+        }
+
+        public synchronized void set(int value) {
+            this.value = value;
+        }
+    }
+
     public static void main(String[] args) {
+
     }
 }
