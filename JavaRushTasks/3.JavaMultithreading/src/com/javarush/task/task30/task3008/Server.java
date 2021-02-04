56fc726055dd3bd01f6534a71689e26f3bc958fe
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-04 20:52:01 +0500

    Modify Server.java by step 6

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,40 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args) {
+        int serverPort = ConsoleHelper.readInt();
+
+        try {
+            ServerSocket socket = new ServerSocket(serverPort);
+            System.out.println("Сервер запущен.");
+            while (true){
+                try {
+                    Socket client = socket.accept();
+                    if (client != null){
+                        Handler handler = new Handler(client);
+                        handler.start();
+                    }
+                } catch (Exception e){
+                    socket.close();
+                    System.out.println("Ошибка сервера.");
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
