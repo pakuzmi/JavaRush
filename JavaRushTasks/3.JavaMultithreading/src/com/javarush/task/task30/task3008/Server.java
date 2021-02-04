f693e79692c91a19dcd3629834c4a29515901f6b
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-04 20:59:17 +0500

    Modify Server.java by step 7

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,15 +3,18 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
     private static class Handler extends Thread{
         private Socket socket;
-
         public Handler(Socket socket){
             this.socket = socket;
         }
     }
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         int serverPort = ConsoleHelper.readInt();
@@ -35,6 +38,16 @@ public class Server {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String, Connection> pair : connectionMap.entrySet()){
+            try {
+                pair.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println("Не удалось отправить сообщение");
+            }
         }
     }
 }
