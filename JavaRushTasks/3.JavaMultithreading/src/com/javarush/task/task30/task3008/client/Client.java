1a532dcd19b8b1fc7858683686a9830ab8e351ff
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-05 22:55:06 +0500

    Add Client.java by step 13

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,13 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
     protected Connection connection;
@@ -10,4 +17,36 @@ public class Client {
 
     }
 
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Введите адрес сервера.");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введите порт сервера.");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введите имя пользователя.");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+            clientConnected = false;
+        }
+    }
 }
