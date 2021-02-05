94adbb6debc84d45e30e9f10b9686c415d555517
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-05 23:52:07 +0500

    Add Client.java by step 17

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -6,6 +6,7 @@ import com.javarush.task.task30.task3008.Message;
 import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
     protected Connection connection;
@@ -56,6 +57,20 @@ public class Client {
                 } else {
                     throw new IOException("Unexpected MessageType");
                 }
+            }
+        }
+
+        @Override
+        public void run() {
+            String serverAddress = getServerAddress();
+            int serverPort = getServerPort();
+            try {
+                Socket socket = new Socket(serverAddress, serverPort);
+                connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException | ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
             }
         }
     }
