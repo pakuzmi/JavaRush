bcac2d8814a0525125392b167c705c6574515723
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-10 17:20:58 +0500

    Add FileManager.java by step 9

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,19 +1,14 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.Command;
-import com.javarush.task.task31.task3110.command.ExitCommand;
-import com.javarush.task.task31.task3110.exception.*;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         Operation operation = null;
-        while (operation != Operation.EXIT){
+        do {
             try {
                 operation = askOperation();
                 CommandExecutor.execute(operation);
@@ -23,48 +18,20 @@ public class Archiver {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
 
-        }
-/*        System.out.println("Введите полный путь архива в который будем архивировать.");
-        String archivePath = "";
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            archivePath = br.readLine();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        ZipFileManager fileManager = new ZipFileManager(Paths.get(archivePath));
-        System.out.println("Введите полный путь к файлу, который будем архивировать.");
-        String filePath = "";
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            filePath = br.readLine();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        try {
-            fileManager.createZip(Paths.get(filePath));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Command exitCommand = new ExitCommand();
-        try {
-            exitCommand.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        } while (operation != Operation.EXIT);
     }
 
-    public static Operation askOperation() throws IOException{
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
         ConsoleHelper.writeMessage("Выберите операцию:");
-        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
-        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
-        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
-        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
-        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
-        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
-        int numOperation = ConsoleHelper.readInt();
-        for (Operation operation: Operation.values()){
-            if (operation.ordinal() == numOperation) return operation;
-        }
-        return null;
-    }
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
 
+        return Operation.values()[ConsoleHelper.readInt()];
+    }
 }
