4535e505df8396043841ac25f98dbc63c7ed9fe4
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-11 16:17:36 +0500

    Modify FileProperties.java by step 12

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -7,11 +7,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
+
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь файла будущего архива.");
-        String filePath = ConsoleHelper.readString();
-        Path path = Paths.get(filePath);
-        ZipFileManager zipFileManager = new ZipFileManager(path);
-        return zipFileManager;
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
