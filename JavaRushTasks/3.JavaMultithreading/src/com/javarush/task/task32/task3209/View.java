5c5d51bb936431ec0d92bd23d4dcedc177b06159
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-16 23:15:32 +0500

    Modify View.java by step 4 Task3209

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
@@ -25,10 +29,23 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init(){
-
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        setVisible(true);
     }
 
     public void exit(){
         controller.exit();
+    }
+
+    public void initMenuBar(){}
+
+    public void initEditor(){}
+
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
     }
 }
