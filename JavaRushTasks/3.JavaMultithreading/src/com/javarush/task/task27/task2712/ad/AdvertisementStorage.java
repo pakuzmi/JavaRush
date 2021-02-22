371c8656bb01d76176487de6ae5e511a821a7a01
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-22 18:55:17 +0500

    Modify AdvertisementManager.java, AdvertisementStorage.java by step7 of Task2712 RestaurantApp

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,9 +1,18 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
     private static AdvertisementStorage advertisementStorage;
+    private final List<Advertisement> videos = new ArrayList<>();
+
 
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60));
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60));
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));
     }
 
     public static AdvertisementStorage getInstance() {
@@ -11,5 +20,13 @@ public class AdvertisementStorage {
             advertisementStorage = new AdvertisementStorage();
         }
         return advertisementStorage;
+    }
+
+    public List list(){
+        return videos;
+    }
+
+    public void add(Advertisement advertisement){
+        videos.add(advertisement);
     }
 }
