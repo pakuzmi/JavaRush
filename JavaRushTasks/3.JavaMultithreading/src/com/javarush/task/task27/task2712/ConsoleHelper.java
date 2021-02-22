d0197a0674cd92044958acd1bd9e5ad5dc4874d2
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-22 12:03:50 +0500

    Add ConsoleHelper.java, modify Order.java, Tablet.java by step2 of Task2712 RestaurantApp

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ConsoleHelper.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task27.task2712;
+
+import com.javarush.task.task27.task2712.kitchen.Dish;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleHelper {
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return reader.readLine();
+    }
+
+    public static List<Dish> getAllDishesForOrder() throws IOException {
+        List<Dish> order = new ArrayList<>();
+        writeMessage("Список всех блюд:");
+        writeMessage("=============================================================");
+        writeMessage(Dish.allDishesToString());
+        writeMessage("=============================================================");
+        writeMessage("Введите блюдо из предложено списка для добавления его в заказ (exit для завершения выбора).");
+        List<String> enumEntryString = new ArrayList<>();
+        for (Dish dish : Dish.values()){
+            enumEntryString.add(dish.name());
+        }
+        while (true) {
+            String choice = readString();
+            if (choice.equals("exit")) {
+                break;
+            }
+            if (enumEntryString.contains(choice)){
+                order.add(Dish.valueOf(choice));
+            } else {
+                writeMessage("Блюда " + choice + " нет в списке, повторите ввод.");
+            }
+        }
+        return order;
+    }
+}
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -1,13 +1,36 @@
 package com.javarush.task.task27.task2712.kitchen;
+import com.javarush.task.task27.task2712.ConsoleHelper;
 import com.javarush.task.task27.task2712.Tablet;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Order {
     private final Tablet tablet;
     protected List<Dish> dishes;
 
-    public Order(Tablet tablet) {
+    public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
+        dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+
+    @Override
+    public String toString() {
+        if (dishes.size() > 0){
+        StringBuilder result = new StringBuilder();
+        result.append("Your order: [");
+        int i = 1;
+        for (Dish dish : dishes){
+            result.append(dish.name());
+            if (i < dishes.size()){
+                result.append(", ");
+            }
+            i++;
+        }
+        result.append("] of ");
+        result.append(tablet.toString());
+        return result.toString();
+        }
+        return "";
     }
 }
