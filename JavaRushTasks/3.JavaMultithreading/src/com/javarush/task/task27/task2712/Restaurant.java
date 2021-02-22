b3be79c660aeeb6bbed223957440492938f6f440
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-22 18:09:07 +0500

    Add Waiter.java, modify Restaurant.java by step4 of Task2712 RestaurantApp

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,11 +1,15 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(12);
-        tablet.addObserver(new Cook("Amigo"));
+        Cook cook = new Cook("Amigo");
+        cook.addObserver(new Waiter());
+        tablet.addObserver(cook);
+
         tablet.createOrder();
         tablet.createOrder();
         tablet.createOrder();
