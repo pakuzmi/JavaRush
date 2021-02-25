e48f4f697dd65f956892b6e55d50e634c1597b67
Pavel Kuzmin <pakuzmi@gmail.com>
2021-02-25 16:38:40 +0500

    Modify StatisticManager.java by step13 of Task2712 RestaurantApp

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,9 +1,16 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
     private static StatisticManager instance;
+    private StatisticManager.StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {
     }
@@ -19,5 +26,13 @@ public class StatisticManager {
 
     }
 
+    private class StatisticStorage{
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
 
+        public StatisticStorage() {
+            for (EventType eventType : EventType.values()){
+                storage.put(eventType, new ArrayList<EventDataRow>());
+            }
+        }
+    }
 }
