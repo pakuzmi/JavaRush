3664ac214fc1959fc0522490eea18bf85f376069
Pavel Kuzmin <pakuzmi@gmail.com>
2021-03-16 21:04:55 +0500

    Modify Model.java by step4 of my solution Task3513 Game2048.app

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,15 +1,43 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
 
     public Model() {
+        resetGameTiles();
+    }
+
+    private List<Tile> getEmptyTiles(){
+        List<Tile> list = new ArrayList<>();
+        for (int y = 0; y < FIELD_WIDTH; y++){
+            for (int x = 0; x < FIELD_WIDTH; x++){
+                if (gameTiles[y][x].value == 0){
+                    list.add(gameTiles[y][x]);
+                }
+            }
+        }
+        return list;
+    }
+
+    private void addTile(){
+        List<Tile> emptyTiles = getEmptyTiles();
+        if (emptyTiles.size() == 0) return;
+        Tile randomTile = emptyTiles.get((int) (Math.random() * emptyTiles.size()));
+        randomTile.value = Math.random() < 0.9 ? 2 : 4;
+    }
+
+    public void resetGameTiles(){
         for (int y = 0; y < FIELD_WIDTH; y++){
             for (int x = 0; x < FIELD_WIDTH; x++){
                 gameTiles[y][x] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
 }
