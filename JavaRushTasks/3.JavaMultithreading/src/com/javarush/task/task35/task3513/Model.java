5ea5fd73b88f31d45a0db320559cc71db58513d1
Pavel Kuzmin <pakuzmi@gmail.com>
2021-03-16 20:17:08 +0500

    Modify Model.java by step3 of my solution Task3513 Game2048.app

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,15 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+
+    public Model() {
+        for (int y = 0; y < FIELD_WIDTH; y++){
+            for (int x = 0; x < FIELD_WIDTH; x++){
+                gameTiles[y][x] = new Tile();
+            }
+        }
+    }
 }
