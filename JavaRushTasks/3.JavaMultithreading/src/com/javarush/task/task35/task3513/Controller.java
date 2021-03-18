e32d3641876a01cf0166b50e3045ed3e0df79cd7
Pavel Kuzmin <pakuzmi@gmail.com>
2021-03-18 22:33:28 +0500

    Modify Controller.java, Model.java, View.java by step9 of my solution Task3513 Game2048.app

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,14 +1,16 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private static final int WINNING_TILE = 2048;
 
-    public Controller(Model model, View view) {
+    public Controller(Model model) {
         this.model = model;
-        this.view = view;
+        this.view = new View(this);
     }
 
     public Tile[][] getGameTiles(){
@@ -17,5 +19,46 @@ public class Controller extends KeyAdapter {
 
     public int getScore(){
         return model.score;
+    }
+
+    public void resetGame(){
+        model.score = 0;
+        view.isGameLost = false;
+        view.isGameWon = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE){
+            resetGame();
+        }
+        if (!model.canMove()){
+            view.isGameLost = true;
+        }
+        if (!view.isGameLost && !view.isGameWon){
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_LEFT:{
+                    model.left();
+                    break;
+                }
+                case KeyEvent.VK_RIGHT:{
+                    model.right();
+                    break;
+                }
+                case KeyEvent.VK_UP:{
+                    model.up();
+                    break;
+                }
+                case KeyEvent.VK_DOWN:{
+                    model.down();
+                    break;
+                }
+            }
+            if (model.maxTile == WINNING_TILE){
+                view.isGameWon = true;
+            }
+        }
+        view.repaint();
     }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -143,7 +143,7 @@ public class Model {
                 }
             }
         }
-
+                
         return result;
      }
 }
